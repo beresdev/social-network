59e32587ef2545f8c703c595796f29579d626208
beresdev <beresdev@gmail.com>
2023-01-23 21:46:38 -0600

    adding firebase configuration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // Este es el punto de entrada de tu aplicacion
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+import firebaseConfig from "../firebase/firebaseConfig.js";
 import { load } from './lib/router.js';
+
+const app = initializeApp(firebaseConfig);
 
 load()
 
